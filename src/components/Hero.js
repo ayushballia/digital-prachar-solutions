8e089c912531ed92a92f1a91e11c7199e7644313
Ayush Tiwari <ayushtiwari@live.in>
2025-02-02 00:01:12 +0530

    bug fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
       <div className="relative isolate px-6 pt-4 lg:px-8">
         <div
           aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl"
         >
           <div
             style={{
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Example() {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-[#d34c50] hover:bg-[#d34c50] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-[#d34c50] hover:bg-[#d34c50] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white z-40">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
